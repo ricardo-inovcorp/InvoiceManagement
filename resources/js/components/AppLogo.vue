e294a84d41ff3ad377e5bde7db0c6dde008a66a4
doctor-ricco <ricardoaraujo.vet@hotmail.com>
2025-04-09 16:50:29 +0100

    Logo replacement

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import AppLogoIcon from '@/components/AppLogoIcon.vue';
+// Sem necessidade de importar o AppLogoIcon
 </script>
 
 <template>
     <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-        <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
+        <img src="/storage/logo/IMS_logo.png" alt="IMS Logo" class="size-6 object-contain" />
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
         <span class="mb-0.5 truncate font-semibold leading-none">Invoice Management System</span>
